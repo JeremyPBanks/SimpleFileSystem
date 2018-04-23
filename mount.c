a5612835b136a2e3e527e9217849c0fa8f1b5e9b
Lance A. Fletcher <laf224@command.cs.rutgers.edu>
2018-04-23 04:16:56 -0400

    Added mount/unmount shortcuts (Final?)

--- mount.c
+++ mount.c
@@ -5,9 +5,16 @@
 int main(int argc, char *argv[])
 {
 	int stat;
+	char *myFolder = "cd /.freespace && mkdir laf224";
 	char *flat = "cd /.freespace/laf224 && touch testfsfile";
 	char *tmp = "cd /tmp && mkdir laf224 && cd laf224 && mkdir mountdir";
 	char *mount = "cd /ilab/users/laf224/Documents/CS416/Project3/src && ./sfs /.freespace/laf224/testfsfile /tmp/laf224/mountdir";
+
+	if((stat = system(myFolder)) < 0)
+	{
+		printf("Failed on %s\n", myFolder);
+		return stat;
+	}
 
 	if((stat = system(flat)) < 0)
 	{
